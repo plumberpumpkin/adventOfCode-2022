fc57ae70d4191b9ea3e1914a51c5ad3691949fc4
plumberpumpkin <28160748+plumberpumpkin@users.noreply.github.com>
2022-12-02 23:41:11 +0100

    refactoring

--- rustvent_of_code/src/day_two.rs
+++ rustvent_of_code/src/day_two.rs
@@ -3,7 +3,8 @@ use std::io::{self, BufRead, BufReader};
 
 pub fn part_one() {
     //define input file location
-    const INPUT_FILE: &str = r"C:\Users\User\GitHub\adventOfCode-2022\rustvent_of_code\src\inputs\day_two.txt";
+    const INPUT_FILE: &str =
+        r"C:\Users\User\GitHub\adventOfCode-2022\rustvent_of_code\src\inputs\day_two.txt";
 
     //read file
 
@@ -15,25 +16,37 @@ pub fn part_one() {
     let mut score: u32 = 0;
     let mut linecount = 1;
     for line in reader.lines() {
-        let input: Vec<String> = line.unwrap().split_whitespace().map(|s| s.parse().unwrap()).collect();
-        
+        let input: Vec<String> = line
+            .unwrap()
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
+
         //compare the two values
         //A/X => Rock
-        //B/Y => Paper 
+        //B/Y => Paper
         //C/Z => Scissors
 
         let mut win = false;
         let mut draw = false;
         let mut loose = false;
 
-        if input[0] == "A" && input[1] == "Y" || input[0] == "B" && input[1] == "Z" || input[0] == "C" && input[1] == "X"  {
+        if input[0] == "A" && input[1] == "Y"
+            || input[0] == "B" && input[1] == "Z"
+            || input[0] == "C" && input[1] == "X"
+        {
             win = true;
-        } else if input[0] == "A" && input[1] == "X" || input[0] == "B" && input[1] == "Y" || input[0] == "C" && input[1] == "Z"  {
+        } else if input[0] == "A" && input[1] == "X"
+            || input[0] == "B" && input[1] == "Y"
+            || input[0] == "C" && input[1] == "Z"
+        {
             draw = true;
-        } else if input[0] == "A" && input[1] == "Z" || input[0] == "B" && input[1] == "X" || input[0] == "C" && input[1] == "Y"  {
+        } else if input[0] == "A" && input[1] == "Z"
+            || input[0] == "B" && input[1] == "X"
+            || input[0] == "C" && input[1] == "Y"
+        {
             loose = true;
         }
-
 
         //calculate result for this round
         if win {
@@ -46,14 +59,8 @@ pub fn part_one() {
         println!("Current line count: {}", linecount);
         linecount += 1;
         println!("Current score: {}", score);
-        
-            
-    
     }
 
     println!("Your score is: {}", score);
-
-    
-
 }
 pub fn part_two() {}
