dc20aed97947b4b63097f4cc40eefe7cb7f12e0c
plumberpumpkin <28160748+plumberpumpkin@users.noreply.github.com>
2022-12-04 12:58:34 +0100

    day three part one

--- rustvent_of_code/src/day_three.rs
+++ rustvent_of_code/src/day_three.rs
@@ -1,8 +1,21 @@
+use std::io::BufRead;
+
 pub fn part_one() {
     //define input location
     const INPUT_FILE: &str =
         r"C:\Users\User\GitHub\adventOfCode-2022\rustvent_of_code\src\inputs\day_three.txt";
     let file = super::utils::read_input_file(INPUT_FILE);
+
+    //read single lines from file
+    //split line in half
+    //find char that is part of both halfs
+    //push char in intermediate vector
+
+    //initilize intermediate vector
+    let mut relevant_chars: Vec<char> = Vec::new();
+    for line in file.lines(){
+        
+    }
 }
 
 pub fn part_two() {}
--- rustvent_of_code/target/debug/deps/rustvent_of_code-5e46b18579b872c8.d
+++ rustvent_of_code/target/debug/deps/rustvent_of_code-5e46b18579b872c8.d
@@ -1,9 +1,9 @@
-c:\Users\User\GitHub\adventOfCode-2022\rustvent_of_code\target\debug\deps\rustvent_of_code-5e46b18579b872c8.rmeta: src\main.rs src\day_one.rs src\day_two.rs src\day_three.rs src\utils.rs
+c:\Users\User\GitHub\adventOfCode-2022\rustvent_of_code\target\debug\deps\rustvent_of_code-5e46b18579b872c8.rmeta: src\main.rs src\day_one.rs src\day_three.rs src\day_two.rs src\utils.rs
 
-c:\Users\User\GitHub\adventOfCode-2022\rustvent_of_code\target\debug\deps\rustvent_of_code-5e46b18579b872c8.d: src\main.rs src\day_one.rs src\day_two.rs src\day_three.rs src\utils.rs
+c:\Users\User\GitHub\adventOfCode-2022\rustvent_of_code\target\debug\deps\rustvent_of_code-5e46b18579b872c8.d: src\main.rs src\day_one.rs src\day_three.rs src\day_two.rs src\utils.rs
 
 src\main.rs:
 src\day_one.rs:
-src\day_two.rs:
 src\day_three.rs:
+src\day_two.rs:
 src\utils.rs:
