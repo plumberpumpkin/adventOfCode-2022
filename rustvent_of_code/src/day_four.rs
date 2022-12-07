14665824a12d2d497a4885b5d727b2e966f6f6e3
plumberpumpkin <28160748+plumberpumpkin@users.noreply.github.com>
2022-12-07 12:36:29 +0100

    add day four

---
+++ rustvent_of_code/src/day_four.rs
@@ -0,0 +1,18 @@
+use std::io::BufRead;
+
+pub fn part_one(){
+
+    const INPUT_FILE: &str =
+        r"C:\Users\User\GitHub\adventOfCode-2022\rustvent_of_code\src\inputs\day_four.txt";
+    let file = super::utils::read_input_file(INPUT_FILE);
+
+    //initialize relevant variables
+    let mut containing_counter: u32 = 0;
+
+    for line in file.lines(){
+        let assignment_one = line.split('-').unwrap();
+        
+    }
+
+
+}
