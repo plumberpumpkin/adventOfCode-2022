319c1f755542b9aca2a39ca881937caeb5152d41
plumberpumpkin <28160748+plumberpumpkin@users.noreply.github.com>
2022-12-09 19:21:32 +0100

    attempt day five part two

--- rustvent_of_code/src/day_five.rs
+++ rustvent_of_code/src/day_five.rs
@@ -37,14 +37,14 @@ pub fn part_one() {
     }
     //TODO apply instructions
     for step in instructions {
-        let move_count = step[0];
+        let moving_stack_size = step[0];
         let start_stack_number = step[1] - 1;
         let target_stack_number = step[2] - 1;
         let mut start_stack: Vec<char> = supply_stack[start_stack_number].clone();
         let mut target_stack: Vec<char> = supply_stack[target_stack_number].clone();
         let mut moves_done: usize = 0;
 
-        while moves_done < move_count {
+        while moves_done < moving_stack_size {
             let item = start_stack[0];
             target_stack.insert(0, item);
             start_stack.remove(0);
@@ -69,7 +69,89 @@ pub fn part_one() {
         first_crates.insert(count, current_crate);
         count += 1;
     }
-    println!("Fist crates are: {}", first_crates);
+    println!("Part 1: First crates are: {}", first_crates);
 }
 
-pub fn part_two() {}
+pub fn part_two() {
+    const MOVING_INSTRUCTIONS: &str = r"C:\Users\User\GitHub\adventOfCode-2022\rustvent_of_code\src\inputs\moving_instructions.txt";
+    const STARTING_POINT: &str = r"C:\Users\User\GitHub\adventOfCode-2022\rustvent_of_code\src\inputs\starting_point_day_five.txt";
+    let mut supply_stack: Vec<Vec<char>> = Vec::new();
+    let start: BufReader<File> = super::utils::read_input_file(STARTING_POINT);
+    //reading in input matrix
+    for stack in start.lines() {
+        let current_stack: Vec<char> = stack
+            .unwrap()
+            .split(";")
+            .map(|x| x.parse::<char>().unwrap())
+            .collect();
+        supply_stack.push(current_stack);
+    }
+    let mut instructions: Vec<Vec<usize>> = Vec::new();
+    let movement_set = super::utils::read_input_file(MOVING_INSTRUCTIONS);
+    //deconstruct moving instructions
+    for instruction in movement_set.lines() {
+        let current_instruction = instruction.unwrap();
+        let mut work_input_str: Vec<String> = current_instruction
+            .split(" ")
+            .map(|x| x.parse::<String>().unwrap())
+            .collect();
+        //remove strings for further utilization
+        work_input_str.remove(4);
+        work_input_str.remove(2);
+        work_input_str.remove(0);
+        //convert to int value
+        let work_input: Vec<usize> = work_input_str.iter().map(|x| x.parse().unwrap()).collect();
+
+        instructions.push(work_input);
+    }
+
+    let mut current_step = 0;
+
+    for step in instructions {
+        current_step += 1;
+        let moving_stack_size_index = step[0] - 1;
+        let start_stack_number = step[1] - 1;
+        let target_stack_number = step[2] - 1;
+        let mut start_stack: Vec<char> = supply_stack[start_stack_number].clone();
+        let mut target_stack: Vec<char> = supply_stack[target_stack_number].clone();
+        
+
+        //apply instructions
+        //move multiple items at a time according to instruction
+        if start_stack.len() > moving_stack_size_index {
+            let sub_stacks = start_stack.split_at(moving_stack_size_index);
+        let mut moving_items = sub_stacks.0.to_vec();
+        moving_items.reverse();
+        for item in moving_items {
+            target_stack.insert(0, item);
+        }
+        start_stack = sub_stacks.1.to_vec();
+        } else {
+            println!("Attempt ({}) is larger than stack ({}), step is {} ", step[0], start_stack.len(), current_step);
+        }
+        
+
+
+
+
+        let target_stack_to_remove = target_stack_number + 1;
+        let start_stack_to_remove = start_stack_number + 1;
+        //replace targetstack
+        supply_stack.insert(target_stack_number, target_stack);
+        supply_stack.remove(target_stack_to_remove);
+        //replace startstack
+        supply_stack.insert(start_stack_number, start_stack);
+        supply_stack.remove(start_stack_to_remove);
+    }
+
+
+    //Create message for elfs
+    let mut first_crates: String = String::new();
+    let mut count = 0;
+    for stack in supply_stack {
+        let current_crate = stack[0];
+        first_crates.insert(count, current_crate);
+        count += 1;
+    }
+    println!("Part 2: First crates are: {}", first_crates);
+}
--- rustvent_of_code/target/debug/deps/rustvent_of_code.d
+++ rustvent_of_code/target/debug/deps/rustvent_of_code.d
@@ -1,6 +1,6 @@
-C:\Users\User\GitHub\adventOfCode-2022\rustvent_of_code\target\debug\deps\rustvent_of_code.exe: src\main.rs src\day_five.rs src\day_four.rs src\day_one.rs src\day_three.rs src\day_two.rs src\utils.rs
+C:\Users\User\GitHub\adventOfCode-2022\rustvent_of_code\target\debug\deps\rustvent_of_code.exe: src\main.rs src\day_five.rs src\day_four.rs src\day_one.rs src\day_three.rs src\day_two.rs src\day_six.rs src\utils.rs
 
-C:\Users\User\GitHub\adventOfCode-2022\rustvent_of_code\target\debug\deps\rustvent_of_code.d: src\main.rs src\day_five.rs src\day_four.rs src\day_one.rs src\day_three.rs src\day_two.rs src\utils.rs
+C:\Users\User\GitHub\adventOfCode-2022\rustvent_of_code\target\debug\deps\rustvent_of_code.d: src\main.rs src\day_five.rs src\day_four.rs src\day_one.rs src\day_three.rs src\day_two.rs src\day_six.rs src\utils.rs
 
 src\main.rs:
 src\day_five.rs:
@@ -8,4 +8,5 @@ src\day_four.rs:
 src\day_one.rs:
 src\day_three.rs:
 src\day_two.rs:
+src\day_six.rs:
 src\utils.rs:
